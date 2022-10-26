b9176d508e1302f2c37516dacc21b5f629bc2d5e
Anton <antivzzz26@gmail.com>
2022-10-26 21:16:21 +0300

    загрузка стартового шаблона

---
+++ src/js/app.js
@@ -0,0 +1,25 @@
+import * as testWebp from './modules/testWebp.js'
+import {elements} from './modules/elements.js';
+
+testWebp.isWebp(); // проверка поддержки WebP изображений браузером
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
